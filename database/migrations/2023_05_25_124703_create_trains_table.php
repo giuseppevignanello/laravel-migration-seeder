e640f2f561fa03c36767def6c923184942e89c22
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-25 14:59:15 +0200

    add model and migration

---
+++ database/migrations/2023_05_25_124703_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('startStation', 100);
+            $table->string('endStation', 100);
+            $table->time('departureTime');
+            $table->time('arrivalTime');
+            $table->tinyInteger('numberOfCoaches')->unsigned();
+            $table->boolean('onTime')->default(true);
+            $table->boolean('cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
